f20e6ffef176cc34794701b33d89f2dd6e29e48a
abvert <annabertavert@gmail.com>
2016-12-15 19:30:02 +0100

    Rollback previous adjustments.

--- src/main/java/com/vean/helloapsis/resources/HelloApsisIncrementCounterResource.java
+++ src/main/java/com/vean/helloapsis/resources/HelloApsisIncrementCounterResource.java
@@ -37,9 +37,7 @@ public class HelloApsisIncrementCounterResource {
     @Timed
     public CounterInfo sayHelloAndIncrementCounter(@QueryParam("name") Optional<String> name) {
         final String content = String.format(template, name.or(defaultName));
-        if (this.countersStorage.keySet().contains(content))
-            this.countersStorage.computeIfPresent(content, (cont, counter) -> this.countersStorage.get(content)).increment();
-        else
-            this.countersStorage.computeIfAbsent(content, k -> new LongAdder()).increment();
-        return new CounterInfo(content, this.countersStorage.get(content).longValue());    }
+        this.countersStorage.computeIfAbsent(content, k -> new LongAdder()).increment();
+        return new CounterInfo(content, this.countersStorage.get(content).longValue());    
+    }
 }
